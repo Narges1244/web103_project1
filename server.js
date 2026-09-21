8f44c1a6b1d40b8f65b9195ffed79d33750310c7
narges1244 <narges.salari@expressgene.us>
2026-09-20 22:10:19 -0400

    add project explorer homepage

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const   express = require("express");
 
 const app = express();
+app.use(express.static("public"));
 
 const PORT =  3000;
 
-app.get("/", (req,res) => {
-    res.send("welcome to my Project Explorer!");
-});
+
 
 app.listen(PORT, () => {
     console.log(`🎉 Server is running on port ${PORT}`);
