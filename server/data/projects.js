6153f471c2c7c80be9be2212fd4711adbdfb92a9
narges1244 <narges.salari@expressgene.us>
2026-09-20 23:14:33 -0400

    add intial project data

---
+++ server/data/projects.js
@@ -0,0 +1,18 @@
+const projects = [
+    {
+        id: "genetic-data-dashboard",
+        title: "Genetic Data Analysis Dashboard",
+        category: "Full-Stack Development / Bioinformatics",
+        description:
+            "Built a full-stack web application to analyze, manage, and visualize complex genetic sequencing data.",
+        technologies: [
+            "React.js",
+            "Node.js",
+            "Express.js",
+            "PostgreSQL",
+            "AWS"
+        ]
+    }
+];
+
+module.exports = projects;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const   express = require("express");
-
+const path = require("path");
 const app = express();
-app.use(express.static("public"));
+
+app.use(express.static(path.join(__dirname, "../client")));
+
 
 const PORT =  3000;
 
